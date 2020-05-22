01785381bdd31f107fa10f1a3704c7c2cb9d6d01
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-22 12:10:16 +0900

    1부터 10까지 더하기

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -29,5 +29,19 @@ namespace Tests
 
             Assert.AreEqual(expected, c, "빼기 연산 실패!");
         }
+
+        [TestMethod]
+        public void T03_1부터_10까지_더하기()
+        {
+            int result = 0;
+
+            for(int i = 1; i < 11; i++)
+            {
+                result += i;
+            }
+            int expected = 55;
+
+            Assert.AreEqual(expected, result, "더하기 연산 실패!");
+        }
     }
 }
